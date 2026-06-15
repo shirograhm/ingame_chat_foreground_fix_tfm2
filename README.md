e196249149b30ae731307f930f07be2d6e31dc98
shirograhm <xavierwg13@gmail.com>
2026-06-14 19:29:15 -0700

    remove unneeded code mod

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Ingame Chat Foreground Fix - TFM2
+# Ingame Chat Foreground Fix
 
 Fixes the weird overlap with the ingame chat and the champion current info hover information.
--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-use mod_api::*;
-
-fn init(_ctx: &GameCtx) -> ModRegistration {
-    let reg = ModRegistration::new("ingame_chat_foreground_fix_tfm2");
-    reg
-}
-
-declare_mod!(init);
